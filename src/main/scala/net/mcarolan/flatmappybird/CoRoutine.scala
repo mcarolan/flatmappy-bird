73f65168052bc89ceb8e0b976eed220301136cbe
Martin Carolan <martin.carolan@itv.com>
2016-08-30 20:08:18 +0100

    More coroutine stuff

--- src/main/scala/net/mcarolan/flatmappybird/CoRoutine.scala
+++ src/main/scala/net/mcarolan/flatmappybird/CoRoutine.scala
@@ -1,8 +1,28 @@
 package net.mcarolan.flatmappybird
 
-trait CoRoutine[Input, Output] {
+trait CoRoutine[Input, Output] { self =>
 
   def run(input: Input): (Output, CoRoutine[Input, Output])
+
+  def map[OutputT](f: Output => OutputT): CoRoutine[Input, OutputT] =
+    new CoRoutine[Input, OutputT] {
+      override def run(input: Input): (OutputT, CoRoutine[Input, OutputT]) = {
+        val (o, co) = self.run(input)
+        (f(o), co.map(f))
+      }
+    }
+
+  def zip[InputT, OutputT](otherCo: CoRoutine[InputT, OutputT]): CoRoutine[(Input, InputT), (Output, OutputT)] =
+    new CoRoutine[(Input, InputT), (Output, OutputT)] {
+      override def run(input: (Input, InputT)): ((Output, OutputT), CoRoutine[(Input, InputT), (Output, OutputT)]) =
+        input match {
+          case (me, other) => {
+            val (resMe, nextMe) = self.run(me)
+            val (resOther, nextOther) = otherCo.run(other)
+            ((resMe, resOther), nextMe.zip(nextOther))
+          }
+        }
+    }
 
 }
 
@@ -13,5 +33,32 @@ object CoRoutine {
       override def run(input: Input): (Output, CoRoutine[Input, Output]) =
         (f(input), arr(f))
     }
+
+
+  def scan[A, B](f: (A, B) => A, init: A): CoRoutine[B, A] = {
+    def step(a: A)(b: B): (A, CoRoutine[B, A]) = {
+      val next = f(a, b)
+
+      (next, new CoRoutine[B, A] {
+        override def run(input: B): (A, CoRoutine[B, A]) =
+          step(next)(input)
+      })
+    }
+
+    new CoRoutine[B, A] {
+      override def run(input: B): (A, CoRoutine[B, A]) =
+        step(init)(input)
+    }
+  }
+
+  def withPrevious[Input](init: Input): CoRoutine[Input, (Input, Input)] = {
+    def step(prev: Input): CoRoutine[Input, (Input, Input)] =
+      new CoRoutine[Input, (Input, Input)] {
+        override def run(input: Input): ((Input, Input), CoRoutine[Input, (Input, Input)]) =
+          ((prev, input), step(input))
+      }
+
+    step(init)
+  }
 
 }
--- src/test/scala/net/mcarolan/flatmappybird/CoRoutineTest.scala
+++ src/test/scala/net/mcarolan/flatmappybird/CoRoutineTest.scala
@@ -1,9 +1,12 @@
 package net.mcarolan.flatmappybird
 
+import java.time.{LocalDate, LocalTime}
+
 import org.scalatest.FunSuite
 import org.scalatest.Matchers._
 
 import scala.annotation.tailrec
+import scala.util.Random
 
 class CoRoutineTest extends FunSuite {
 
@@ -21,6 +24,35 @@ class CoRoutineTest extends FunSuite {
     iter(co, List.empty, list)
   }
 
+  def evalListWithLast[Input, Output](co: CoRoutine[Input, Output], list: List[Input]): (CoRoutine[Input, Output], List[Output]) = {
+    @tailrec
+    def iter(co: CoRoutine[Input, Output], curr: List[Output], remain: List[Input]): (CoRoutine[Input, Output], List[Output]) =
+      remain match {
+        case Nil => (co, curr.reverse)
+        case x :: xs => {
+          val (o, nextCo) = co.run(x)
+          iter(nextCo, curr.::(o), xs)
+        }
+      }
+
+    iter(co, List.empty, list)
+  }
+
+  def eval[Input, Output](co: CoRoutine[Input, Output], inputs: List[Input]): Option[Output] = {
+    var current = co
+    var i = 0
+    var result: Option[Output] = None
+
+    while (i < inputs.size) {
+      val (res, next) = current.run(inputs(i))
+      current = next
+      result = Some(res)
+      i = i + 1
+    }
+
+    result
+  }
+
   test("arr small example") {
     val co: CoRoutine[Int, Int] = CoRoutine.arr(_ * 2)
     evalList(co, List(1, 2, 3)) shouldBe List(2, 4, 6)
@@ -28,9 +60,79 @@ class CoRoutineTest extends FunSuite {
 
   test("arr large example") {
     val co: CoRoutine[Int, Int] = CoRoutine.arr(_ * 2)
-    evalList(co, (1 to 1000000).toList) should have size(1000000)
+    evalList(co, (1 to 1000000).toList) should have size 1000000
   }
 
+  test("map small example") {
+    val double: CoRoutine[Int, Int] = CoRoutine.arr(_ * 2)
+    val doubleString: CoRoutine[Int, String] = double.map(_.toString)
+
+    evalList(doubleString, List(1, 2, 3)) shouldBe List("2", "4", "6")
+  }
+
+  test("map large example") {
+    val double: CoRoutine[Int, Int] = CoRoutine.arr(_ * 2)
+    val doubleString: CoRoutine[Int, String] = double.map(_.toString)
+
+    evalList(doubleString, (1 to 1000000).toList) should have size 1000000
+  }
+
+  test("accumulation with scan") {
+    case class Person(name: String, age: Int)
+
+    val people = List(Person("Martin", 27), Person("Bob", 20), Person("Alice", 21))
+
+    val ageCounter = CoRoutine.scan[Int, Person]((acc, p) => acc + p.age, 0)
+
+    evalList(ageCounter, people) shouldBe List(27, 47, 68)
+  }
+
+  test("accumulation with scan large") {
+    val list = (0L to 10000L).toList
+    val expectedSum = list.sum
+
+    val sumWithCoRoutine = CoRoutine.scan[Long, Long]((acc, next) => acc + next, 0L)
+
+    eval(sumWithCoRoutine, list) shouldBe Some(expectedSum)
+  }
+
+  test("zip") {
+    val inputA: List[Boolean] = List(true, false, true, true, false)
+    val inputB: List[Int] = (1 to 5).toList
+
+    val numberOfTrues: CoRoutine[Boolean, Int] =
+      CoRoutine.scan((acc, next) => if (next) acc + 1 else acc, 0)
+
+    val lastEvenAsString: CoRoutine[Int, String] =
+      CoRoutine.scan((lastEven, current) => if (current % 2 == 0) current.toString else lastEven, "none")
+
+    val truesAndLastEven: CoRoutine[(Boolean, Int), (Int, String)] =
+      numberOfTrues zip lastEvenAsString
+
+    val input = inputA.zip(inputB)
+
+    eval(truesAndLastEven, input) shouldBe Some((3, "4"))
+  }
+
+  test("withPrevious") {
+    val inputs = (1 to 3).toList
+    evalList(CoRoutine.withPrevious(0), inputs) shouldBe List((0, 1), (1, 2), (2, 3))
+  }
+
+  test("withPrevious continuation") {
+    val inputs = (1 to 100000).toList
+
+    println(LocalTime.now().toString)
+
+    val (nextCo, firstResults) = evalListWithLast(CoRoutine.withPrevious(0), inputs)
+    firstResults.last shouldBe (99999, 100000)
+
+    println(LocalTime.now().toString)
+    val nextInputs = List(5)
+
+    eval(nextCo, nextInputs) shouldBe Some((100000, 5))
+    println(LocalTime.now().toString)
+  }
 
 
 }
